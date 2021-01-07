23b52f8065cd604b2993af56aded074fcd999443
bradenslsb <bbir1199@gmail.com>
2021-01-07 10:59:32 -0700

    This is the front end beginning should've committed sooner

--- src/index.js
+++ src/index.js
@@ -1,17 +1,70 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import axios from "axios"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import "./styles.css"
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      todo: "",
+      todos: [],
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  componentDidMount(){
+    axios
+      .get("http://bgb-todo-api.herokuapp.com/todos")
+      .then((res) => {
+        this.setState({
+          todos: res.data
+        })
+      })
+      .catch((err) => console.error(err))
+  }
+  handleChange = e => {
+    this.setState({
+      todo: e.target.value,
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    console.log("submitted")
+  }
+
+  renderTodos = () => {
+    return this.state.todos.map(todo => {
+      return (
+        <div>
+          <h1>{todo.title}</h1>
+        </div>
+      )
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello from app</h1>
+        <form className="add-todo" onSubmit={this.handleSubmit}>
+          <input
+          type="text"
+          placeholder="Add Todo"
+          onChange={this.handleChange}
+          value={this.state.todo}
+          />
+          <button type="submit">Add</button>
+        </form>
+
+        {this.renderTodos}
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
